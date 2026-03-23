79ba6b907815299695d6847125e8e2dc501d4c7a
aokitw513 <asdwkuiop@gmail.com>
2026-03-23 12:38:33 +0800

    Fix: SomeBugs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,31 +1,61 @@
+using Mono.Cecil.Cil;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.iOS;
 
 public class PlayerController : MonoBehaviour
 {
     private float speed = 10f;
     private float jumpForce = 3f;
     private Vector2 moveInput = Vector2.zero;
-    private float mouseSensitivityX = 0.1f;
     private float gamepadSensitivityX = 500f;
-    private float xRotation = 0f;
+    private float yRotation = 0f;
     private Vector2 lookInput = Vector2.zero;
     private Rigidbody rb;
+    private PlayerInput playerInput;
+    private string playerDevice;
+    private Plane plane;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        Cursor.lockState = CursorLockMode.Locked;
+        playerInput = GetComponent<PlayerInput>();
+        playerDevice = playerInput.currentControlScheme;
+        Debug.Log($"Using: {playerDevice}");
+    }
+
+    private void Start()
+    {
+        if(playerDevice == "Keyboard&Mouse")
+        {
+            plane = new Plane(Vector3.up, Vector3.zero);
+        }
     }
 
     private void Update()
     {   
         Vector3 move = new Vector3(moveInput.x, 0f ,moveInput.y) * speed * Time.deltaTime;
-        transform.Translate(move, Space.Self);
+        transform.Translate(move, Space.World);
 
-        xRotation -= lookInput.y;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-    }
+        if (playerDevice == "Gamepad")
+        {
+            yRotation += lookInput.x;
+            transform.localRotation = Quaternion.Euler(0f, yRotation, 0f);
+        }
+        else if(playerDevice == "Keyboard&Mouse")
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+            if(plane.Raycast(ray, out float distance))
+            {
+                Vector3 point = ray.GetPoint(distance);
+
+                point.y = transform.position.y;
+
+                transform.LookAt(point);
+            }
+        }
+   }
 
     // private void FixedUpdate()
     // {
@@ -44,14 +74,9 @@ public class PlayerController : MonoBehaviour
 
     public void OnLook(InputAction.CallbackContext callbackContext)
     {
-        var device = callbackContext.control.device;
         Vector2 rawInput = callbackContext.ReadValue<Vector2>();
 
-        if (device is Mouse)
-        {
-            lookInput = new Vector2(rawInput.x * mouseSensitivityX, 0);
-        }
-        else if (device is Gamepad)
+        if (playerDevice == "Gamepad")
         {
             lookInput = new Vector2(rawInput.x * Time.deltaTime * gamepadSensitivityX, 0);
         }
