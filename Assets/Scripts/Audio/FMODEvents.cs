add5013ec7fd3acada350db71debf65c49812004
aokitw513 <asdwkuiop@gmail.com>
2026-05-13 09:34:03 +0800

    [Add] FMOD Project

--- Assets/Scripts/Audio/FMODEvents.cs
+++ Assets/Scripts/Audio/FMODEvents.cs
@@ -11,20 +11,10 @@ public class FMODEvents : MonoBehaviour
     [field: SerializeField] public EventReference chargeSFX { get; private set; }
     [field: SerializeField] public EventReference dashSFX { get; private set; }
     [field: SerializeField] public EventReference getItemSFX { get; private set; }
+    [field: SerializeField] public EventReference countDownSFX { get; private set; }
+
     [field: Header("BGM")]
     [field: SerializeField] public EventReference BGM { get; private set; }
-
-    [field: Header("Game03")]
-    [field: SerializeField] public EventReference game03Do { get; private set; }
-    [field: SerializeField] public EventReference game03Re { get; private set; }
-    [field: SerializeField] public EventReference game03Mi { get; private set; }
-    [field: SerializeField] public EventReference game03Fa { get; private set; }
-    [field: SerializeField] public EventReference game03Sol { get; private set; }
-    [field: SerializeField] public EventReference game03La { get; private set; }
-    [field: SerializeField] public EventReference game03Si { get; private set; }
-
-    [field: Header("Game05")]
-    [field: SerializeField] public EventReference game05FlipCardSFX { get; private set; }
 
     private void Awake()
     {
